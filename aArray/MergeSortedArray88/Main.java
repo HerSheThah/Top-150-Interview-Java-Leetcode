5ed625df60f0fe8ef5c06f74b12888365f39ec60
kshar <ksharshithah@gmail.com>
2025-02-02 18:25:55 +0530

    added 2 array easy

--- aArray/MergeSortedArray88/Main.java
+++ aArray/MergeSortedArray88/Main.java
@@ -1,7 +1,35 @@
 package aArray.MergeSortedArray88;
 
+import java.util.Arrays;
+
 public class Main {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+            if(n==0)
+                return;
+            int endIndex= m+n-1;
+            while(m>0 && n>0){
+                if(nums1[m-1] > nums2[n-1]){
+                    nums1[endIndex]= nums1[m-1];
+                    m--;
+                }
+                else{
+                    nums1[endIndex]=nums2[n-1];
+                    n--;
+                }
+                endIndex--;
+            }
+            while(n>0){
+                nums1[endIndex--]= nums2[n-1];
+                n--;
+            }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Success");
+        int[] nums2 = new int[]{2, 5, 6};
+        int[] nums1 = new int[]{1, 2, 3,0, 0, 0};
+        int m=3;
+        int n=3;
+        merge(nums1, m, nums2, n);
+        System.out.println(Arrays.toString(nums1));
     }
 }
--- aArray/RemoveElement27/Main.java
+++ aArray/RemoveElement27/Main.java
@@ -1,7 +1,21 @@
 package aArray.RemoveElement27;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class Main {
+    public static int removeElement(int[] nums, int val) {
+        int i=0;
+        for(int k=0; k<nums.length;k++){
+            if(nums[k] != val){
+                nums[i++]= nums[k];
+            }
+        }
+        return i;
+    }
+    public static void main(String[] args) {
+        int[] nums = new int[]{0,1,2,2,3,0,4,2};
+        int val=2;
+        System.out.println(removeElement(nums, val));
+        System.out.println(Arrays.toString(nums));
     }
 }
