ac241b34b5355a57dfac350c5d90c903ce9df5a7
kshar <ksharshithah@gmail.com>
2025-02-06 00:35:24 +0530

    Initial Array medium folders

---
+++ aArray/Medium/BestTimeToStock122/Main.java
@@ -0,0 +1,7 @@
+package aArray.Medium.BestTimeToStock122;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums= new int[]{1, 2, 3, 4};
+    }
+}
---
+++ aArray/Medium/JumpGame55/Main.java
@@ -0,0 +1,7 @@
+package aArray.Medium.JumpGame55;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums= new int[]{1, 2, 3, 4};
+    }
+}
